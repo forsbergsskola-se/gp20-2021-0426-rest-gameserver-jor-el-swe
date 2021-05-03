30477f1aad938cf56302a90eaff7e5c27642029f
Joel Ring <joel.ring@gmail.com>
2021-05-03 23:08:30 +0200

    changes to show my repositories

--- GameRestAPI/Program.cs
+++ GameRestAPI/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text.Json;
@@ -10,27 +9,38 @@ namespace GameRestAPI
 {
     class Program
     {
-        private static readonly HttpClient client = new HttpClient();
+        static readonly HttpClient client = new HttpClient();
         static async Task Main(string[] args)
         {
-            await ProcessRepositories();
-        }
-        
-        private static async Task ProcessRepositories()
-        {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
-
-            var streamTask = client.GetStreamAsync("https://api.github.com/orgs/dotnet/repos");
-            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
-
+            Console.ForegroundColor = ConsoleColor.Green;
+            var repositories = await ProcessRepositories();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("jor-el:s repos");
+            Console.WriteLine("**************************");
             foreach (var repo in repositories) {
-                Console.WriteLine(repo.name);
+                Console.WriteLine(repo.Name);
+                Console.WriteLine(repo.Description);
+                Console.WriteLine(repo.GitHubHomeUrl);
+                Console.WriteLine(repo.Homepage);
+                Console.WriteLine(repo.Watchers);
+                Console.WriteLine(repo.LastPush);
+                Console.WriteLine();
                 
             }
         }
+        
+        static async Task<List<Repository>> ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", "my repo finder");
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/users/jor-el-swe/repos");
+            var repositories = await JsonSerializer.DeserializeAsync<List<Repository>>(await streamTask);
+
+            return repositories;
+ 
+        }
     }
-    
-    
 }
