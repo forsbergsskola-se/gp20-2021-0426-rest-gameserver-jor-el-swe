711bea46faecd5c40d6d0eceea44c88a05dd893c
Joel Ring <joel.ring@gmail.com>
2021-05-06 21:35:23 +0200

    adds conventionpacks to make mongodb implementation work

--- LameScooter/MongoDBLameScooterRental.cs
+++ LameScooter/MongoDBLameScooterRental.cs
@@ -1,13 +1,37 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Conventions;
 using MongoDB.Driver;
 
 namespace LameScooter {
     public class MongoDBLameScooterRental: ILameScooterRental {
         public async Task<int> GetScooterCountInStation(string stationName) {
-            var client = new MongoClient();
             
-            //Mongo-Collection named lamescooters.
-            return 0;
+            if (stationName.Any(char.IsDigit))
+            {
+                throw new ArgumentException("the station name must not contain a number");
+            }
+            
+            var mongoClient = new MongoClient();
+            var database = mongoClient.GetDatabase("lamescooters");
+            var collection = database.GetCollection<BsonDocument>("lamescooters");
+            AddConventionPacks();
+            var filter = Builders<BsonDocument>.Filter.Eq("name", stationName);
+            var document = collection.Find(filter).First();
+            var result = BsonSerializer.Deserialize<LameScooterStationList>(document);
+            return result.BikesAvailable;
+        }
+
+        void AddConventionPacks() {
+            var conventionPack = new ConventionPack {new CamelCaseElementNameConvention()};
+            ConventionRegistry.Register("camelCase", conventionPack, t => true);
+            
+            conventionPack = new ConventionPack { new IgnoreExtraElementsConvention(true) };
+            ConventionRegistry.Register("IgnoreExtraElements", conventionPack, type => true);
         }
     }
 }
