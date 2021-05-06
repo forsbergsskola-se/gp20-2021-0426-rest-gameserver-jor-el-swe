a44ea8ca5a57bcd309f9ecea9f9a2a4b290fdf88
Joel Ring <joel.ring@gmail.com>
2021-05-06 13:29:50 +0200

    adds RealTimeLameScooterRental class

---
+++ LameScooter/RealTimeLameScooterRental.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace LameScooter {
+    public class RealTimeLameScooterRental: ILameScooterRental {
+        public Task<int> GetScooterCountInStation(string stationName) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
