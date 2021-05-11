e1e46cec900de790a8a97fa43a3b3306328b72ae
Joel Ring <joel.ring@gmail.com>
2021-05-11 12:29:27 +0200

    implements playerController

--- MMORPG/PlayersController.cs
+++ MMORPG/PlayersController.cs
@@ -3,31 +3,30 @@ using System.Threading.Tasks;
 
 namespace MMORPG {
     public class PlayersController {
-        readonly Player _player;
         IRepository _repository;
-        Player[] _players;
         public PlayersController(IRepository repository) {
             _repository = repository;
         }
         
-        public async Task<Player> Get(Guid id) {
-            return _player;
+        public Task<Player> Get(Guid id) {
+            return _repository.Get(id);
         }
 
-        public async Task<Player[]> GetAll() {
-            return _players;
+        public Task<Player[]> GetAll() {
+            return _repository.GetAll();
         }
 
-        public async Task<Player> Create(NewPlayer player) {
-            return _player;
+        public Task<Player> Create(NewPlayer player) {
+            var newPlayer = new Player {Name = player.Name, CreationTime = DateTime.Now};
+            return _repository.Create(newPlayer);
         }
 
-        public async Task<Player> Modify(Guid id, ModifiedPlayer player) {
-            return _player;
+        public Task<Player> Modify(Guid id, ModifiedPlayer player) {
+            return _repository.Modify(id, player);
         }
 
-        public async Task<Player> Delete(Guid id) {
-            return _player;
+        public Task<Player> Delete(Guid id) {
+            return _repository.Delete(id);
         }
     }
 }
