aeb5c4603173a202e8ab9d61076afd72b00602e1
Joel Ring <joel.ring@gmail.com>
2021-05-11 11:31:32 +0200

    creates file repository (step 2)

---
+++ MMORPG/FileRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MMORPG {
+    public class FileRepository: IRepository {
+        const string TextFile = "game-dev.txt";
+        public Task<Player> Get(Guid id) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player[]> GetAll() {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> Create(Player player) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> Modify(Guid id, ModifiedPlayer player) {
+            throw new NotImplementedException();
+        }
+
+        public Task<Player> Delete(Guid id) {
+            throw new NotImplementedException();
+        }
+    }
+}
