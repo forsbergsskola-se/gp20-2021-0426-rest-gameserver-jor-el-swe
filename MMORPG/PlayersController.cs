061351ad3eec8215e0f34244047fb3642c7c05b4
Joel Ring <joel.ring@gmail.com>
2021-05-12 17:03:14 +0200

    adds http attributes to player controller

--- MMORPG/PlayersController.cs
+++ MMORPG/PlayersController.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MMORPG {
-    public class PlayersController {
+    //TODO ask marc
+    [ApiController]
+    [Route("[controller]")]
+    public class PlayersController:ControllerBase {
         IRepository _repository;
         public PlayersController(IRepository repository) {
             _repository = repository;
         }
         
+        [HttpGet("player/GetPlayer")]
         public Task<Player> Get(Guid id) {
             return _repository.Get(id);
         }
 
+        [HttpGet("player/GetAllPlayers")]
         public Task<Player[]> GetAll() {
             return _repository.GetAll();
         }
-
+        [HttpPost("player/CreateNewPlayer")]
         public Task<Player> Create(NewPlayer player) {
             var newPlayer = new Player {Name = player.Name, CreationTime = DateTime.Now};
             return _repository.Create(newPlayer);
         }
-
+        [HttpPut("player/ModifyPlayer")]
         public Task<Player> Modify(Guid id, ModifiedPlayer player) {
             return _repository.Modify(id, player);
         }
-
+        [HttpDelete("player/DeletePlayer")]
         public Task<Player> Delete(Guid id) {
             return _repository.Delete(id);
         }
